230cf8a3df1c86258bc28a6e663a825e578977ae
lowbow <lowbow@DESKTOP-AMI2DPG>
2026-09-18 14:11:20 +0900

    ケース１テスト

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,8 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト ログイン機能①
@@ -35,7 +38,20 @@ public class Case01 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		// テストNo.1 トップページURLにアクセス
+		goTo("http://localhost:8080/lms");
+
+		// テストNo.2 ログイン画面のタイトルを確認
+		WebElement title = webDriver.findElement(By.tagName("h2"));
+		assertEquals("ログイン", title.getText());
+
+		// テストNo.3 ログインボタンを確認
+		WebElement loginButton = webDriver.findElement(By.cssSelector("input[type='submit']"));
+		assertEquals("ログイン", loginButton.getAttribute("value"));
+
+		// エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 }
